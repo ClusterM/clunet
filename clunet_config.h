39ac3ea3b16d7669317b225b687de9c390467d67
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2013-09-09 13:22:26 +0400

    CLUNET_ENABLE_INT is deprecated now

--- clunet_config.h
+++ clunet_config.h
@@ -52,9 +52,8 @@
 #define CLUNET_ENABLE_TIMER_OVF set_bit(TIMSK, TOIE2)
 #define CLUNET_DISABLE_TIMER_OVF unset_bit(TIMSK, TOIE2)
 
-/* How to init and enable external interrupr (read pin) */
-#define CLUNET_INIT_INT {set_bit(MCUCR,ISC00);unset_bit(MCUCR,ISC01);}
-#define CLUNET_ENABLE_INT set_bit(GICR, INT0)
+/* How to init and enable external interrupt (read pin) */
+#define CLUNET_INIT_INT {set_bit(MCUCR,ISC00);unset_bit(MCUCR,ISC01); set_bit(GICR, INT0);}
 
 /* Interrupt vectors */
 #define CLUNET_TIMER_COMP_VECTOR TIMER2_COMP_vect
